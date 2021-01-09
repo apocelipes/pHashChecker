53975579f8447494c3344f2652c0e600655f7446
apocelipes <seve3r@outlook.com>
2021-01-09 22:07:15 +0900

    support high dpi

--- main.cpp
+++ main.cpp
@@ -4,6 +4,8 @@
 
 int main(int argc, char *argv[])
 {
+    QApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
+    QApplication::setAttribute(Qt::AA_UseHighDpiPixmaps);
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
